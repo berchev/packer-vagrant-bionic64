49dbb004e0c4be54283b1f60f66f3d6f466c4890
Georgi Berchev <g.berchev@gmail.com>
2021-02-26 11:00:39 +0200

    some change

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -28,7 +28,7 @@ apt-get install -y build-essential ${APTARGS}
 apt-get install -y thin-provisioning-tools ${APTARGS}
 
 # some tools
-apt-get install -y ${APTARGS} python-pip python3-pip git jq curl wget vim language-pack-en sysstat htop
+apt-get install -y ${APTARGS} python-pip python3-pip git jq curl wget vim language-pack-en sysstat htop tree
 
 
 # prep for LXD
